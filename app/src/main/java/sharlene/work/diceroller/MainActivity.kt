1c3d43c43c652dd16ef344aeeb4b129aeb67ed81
Crackmindedsk <shakumbhar@gmail.com>
2021-06-09 21:56:30 +0530

    Banner Ad integrated

--- app/src/main/java/sharlene/work/diceroller/MainActivity.kt
+++ app/src/main/java/sharlene/work/diceroller/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import sharlene.work.diceroller.R.id.button
 
 /**
@@ -12,9 +15,16 @@ import sharlene.work.diceroller.R.id.button
  */
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mAdView:AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this){}
+        mAdView=findViewById(R.id.adView)
+        val adRequest=AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         val rollButton: Button = findViewById(button)
         rollButton.setOnClickListener { rollDice() }
